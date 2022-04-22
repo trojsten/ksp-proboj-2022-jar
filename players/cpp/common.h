a1eb47a50b07ec764a7e49b57af1d6e9a2086b44
Matej Novota <matej.novota@gmail.com>
2022-04-22 13:31:57 +0200

    krtko-update: touch up cpp

--- players/cpp/common.h
+++ players/cpp/common.h
@@ -6,41 +6,52 @@
 #include <vector>
 
 struct Player {
-    int x, y, dx, dy, speed, speed_reset_time, powerup;
-    bool alive;
-    Player () {}
-    Player (std::istream& is) {
-        is >> x >> y >> dx >> dy >> speed >> speed_reset_time >> alive >> powerup;
-    }
+	int x, y, dx, dy, speed, speed_reset_time, powerup;
+	bool alive;
+
+	void update ();
 };
 
 struct PowerUp {
-    int x, y;
-};
+	int x, y;
 
-struct Map {
-    int width, height;
-    std::vector<std::vector<bool>> contents;
-    std::vector<PowerUp> powerups;
-};
-
-enum Direction: char {
-    LEFT = 'L',
-    RIGHT = 'R',
-    NONE = 'x'
+	void update ();
 };
 
 struct Command {
-    Direction direction;
-    bool use_powerup;
+	char direction;
+	bool use_powerup;
 };
 
-Player player_from_input();
+//dalej citat pravdepodobne naozaj nemusis
 
-void read_state();
+void read_state ();
 
-void greet_server(const char *name, const char *color);
+void greet_server (const char *name, const char *color);
 
-void send_command(const Command cmd);
+void send_command (const Command& cmd);
+
+void end_communication ();
+
+template<class T>
+void update (std::vector<T>& vec) {
+	int n;
+	std::cin >> n;
+	if (vec.empty()) vec.assign(n, T());
+	for (int i = 0; i < n; ++i) vec[i].update();
+}
+
+template<class T>
+void update (std::vector<std::vector<T>>& vec) {
+	int w, h;
+	std::cin >> w >> h;
+	if (vec.empty()) vec.assign(w, std::vector<T>(h, T()));
+	for (int y = 0; y < h; ++y) for (int x = 0; x < w; ++x) std::cin >> vec[x][y];
+}
+
+template<class T>
+void update (T& p) {
+	p.update();
+}
 
 #endif
--- players/cpp/main.cpp
+++ players/cpp/main.cpp
@@ -1,77 +1,89 @@
 #include "common.h"
 
+// pohyb
+#define LEFT    -1
+#define RIGHT   1
+#define FORWARD 0
+
+// powerupy Player.powerup
+#define NO_POWERUP   0
+#define SPEED_ME     1
+#define SPEED_OTHERS 2
+#define STOP_ME      3
+#define STOP_OTHERS  4
+#define CLEAN        5
+
 #define NAME "cpp"
 #define COLOR "#00ff00"
 
+int  DX[] = { 0, 1, 0, -1};
+int  DY[] = {-1, 0, 1,  0};
+char DS[] = {LEFT, FORWARD, RIGHT};
 
-Player MYSELF;
-Map MAP;
-std::vector<Player> PLAYERS;
+int width, height; 
+Player myself;
+std::vector<std::vector<int>> walls;
+std::vector<PowerUp> powerups;
+std::vector<Player> players;
 
-void read_state() {
-    std::cin >> MAP.width >> MAP.height;
-    if (MAP.contents.empty()) {
-        for (int x = 0; x < MAP.width; ++x) {
-            std::vector<bool> row;
-            for (int y = 0; y < MAP.height; ++y) {
-                row.push_back(false);
-            }
-            MAP.contents.push_back(row);
-        }
-    }
-
-    for (int y = 0; y < MAP.height; ++y) {
-        for (int x = 0; x < MAP.width; ++x) {
-            bool a;
-            std::cin >> a;
-            MAP.contents[x][y] = a;
-        }
-    }
-
-    MYSELF = player_from_input();
-    int players;
-    std::cin >> players;
-    PLAYERS.clear();
-    for (int i = 0; i < players; ++i) {
-        PLAYERS.push_back(player_from_input());
-    }
-
-    int powerups;
-    std::cin >> powerups;
-    MAP.powerups.clear();
-    for (int i = 0; i < powerups; ++i) {
-        PowerUp p;
-        std::cin >> p.x >> p.y;
-        MAP.powerups.push_back(p);
-    }
-
-    char dot;
-    std::cin >> dot;
+// vrati smer ktorym sme prave otoceny
+int get_my_direction() {
+	for (int i = 0; i < 4; ++i)
+		if(myself.dx == DX[i] && myself.dy == DY[i])
+			return i;
+	return -1;
 }
 
+// skontrolujem ci sa pozeram do pola a ci je v nom 0
+bool is_free(int x, int y) {
+	return x >= 0 && x < width && y >= 0 && y < height && !walls[x][y];
+}
+
+void read_state() {
+	update(walls);
+	update(myself);
+	update(players);
+	update(powerups);
+
+	end_communication();
+	width = walls.size();
+	height = walls[0].size();
+}
+
+
+// sem pis svoj kod
 Command do_turn() {
-    int x = rand() % 3;
-    Command cmd;
-    if (x == 0) {
-        cmd.direction = LEFT;
-    } else if (x == 1) {
-        cmd.direction = RIGHT;
-    } else {
-        cmd.direction = NONE;
-    }
-    return cmd;
+	int x, y;
+	Command cmd;
+	// ak mam powerup pouzijem ho
+	if (myself.powerup != NO_POWERUP) cmd.use_powerup = true;
+	
+	int direction = get_my_direction();
+	// vyskusam pohyb vsetkymi moznymi smermi
+	for (int i = 0; i < 3; ++i) {
+		// zratam potencionalnu polohu
+		x = myself.x + DX[(direction+DS[i]+4)%4];
+		y = myself.y + DY[(direction+DS[i]+4)%4];
+		// skontrolujem ci je volne
+		if (is_free(x,y)) {
+			cmd.direction = DS[i];
+		}
+	}
+	// vypisem si kam sa chcem hybat lebo mozem
+	std::cerr << x << ' ' << y << '\n'; 
+	return cmd;
 }
 
 int main() {
-    srand(time(0));
-
-    greet_server(NAME, COLOR);
-
-    MYSELF.alive = true;
-    while (MYSELF.alive) {
-        read_state();
-        Command cmd = do_turn();
-        send_command(cmd);
-    }
-    std::cout << "Bye\n";
+	// aby sme mali nahodu
+	srand(time(0));
+	// povieme serveru ako sa chceme volat a farbu
+	greet_server(NAME, COLOR);
+	// robime tahy kym sme zivy
+	myself.alive = true;
+	while (myself.alive) {
+		read_state();
+		send_command(do_turn());
+	}
+	std::cout << "Bye\n";
 }
